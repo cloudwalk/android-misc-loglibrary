8dd31246faf9a42ff5e7c1cb32b066bac8d576fd
Maurício Spinardi <mauricio.spinardi@cloudwalk.io>
2021-09-24 12:10:27 -0300

    remove code out of scope

--- LogLibrary/src/androidTest/java/io/cloudwalk/pos/loglibrary/ExampleInstrumentedTest.java
+++ LogLibrary/src/androidTest/java/io/cloudwalk/pos/loglibrary/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package io.cloudwalk.pos.loglibrary;
+package io.cloudwalk.loglibrary;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("io.cloudwalk.pos.loglibrary.test", appContext.getPackageName());
+        assertEquals("io.cloudwalk.loglibrary.test", appContext.getPackageName());
     }
 }
